8acbe3f5e365c395fdfcbfc2fcac6aaa3842a01b
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-06-23 13:30:09 -0400

    firstAndLast.js

--- firstAndLast.js
+++ firstAndLast.js
@@ -1,7 +1,7 @@
  function removeChar(str) {
      let wordsArray = str.split(" ")
      // wordsArray = wordsArray.splice(1,4);
-     wordsArray2 = wordsArray.splice(1, 4);
+     wordsArray = wordsArray.splice(1, 4);
 
      return wordsArray;
  }
